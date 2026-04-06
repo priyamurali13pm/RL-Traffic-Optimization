6894d3af064d0b8d1a3b9828807b9b3e6cd99b14
priya darshini <priyabharani13pm@gmail.com>
2026-04-07 00:35:56 +0530

    Fixed reset structure for OpenEnv compliance

--- openenv.py
+++ openenv.py
@@ -1,18 +1,33 @@
 class Env:
     def __init__(self):
         self.state = [0, 0, 0, 0]
+        self.step_count = 0
+        self.max_steps = 50
 
     def reset(self):
+        self.state = [0, 0, 0, 0]
+        self.step_count = 0
+
         return {
             "observation": self.state,
+            "reward": 0.0,
+            "terminated": False,
+            "truncated": False,
             "info": {}
         }
 
     def step(self, action):
+        self.step_count += 1
+
+        # simple transition
+        self.state = [min(x + 1, 10) for x in self.state]
+
+        done = self.step_count >= self.max_steps
+
         return {
-            "observation": [1, 2, 3, 4],
+            "observation": self.state,
             "reward": 1.0,
-            "terminated": False,
+            "terminated": done,
             "truncated": False,
             "info": {}
         }
