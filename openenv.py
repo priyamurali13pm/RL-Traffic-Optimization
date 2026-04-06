985c97e64bbed7ccacf339b19341d27e13bb4b3f
priya darshini <priyabharani13pm@gmail.com>
2026-04-06 23:08:40 +0530

    Minimal OpenEnv fix

--- openenv.py
+++ openenv.py
@@ -1,26 +1,15 @@
 class Env:
-    def __init__(self):
-        self.state = [0, 0, 0, 0]
-        self.steps = 0
-
     def reset(self):
-        self.state = [0, 0, 0, 0]
-        self.steps = 0
-
         return {
-            "observation": self.state,
+            "observation": [0, 0, 0, 0],
             "info": {}
         }
 
     def step(self, action):
-        self.steps += 1
-
-        self.state = [1, 2, 3, 4]
-
         return {
-            "observation": self.state,
+            "observation": [1, 2, 3, 4],
             "reward": 1.0,
-            "terminated": self.steps >= 50,
+            "terminated": False,
             "truncated": False,
             "info": {}
         }
